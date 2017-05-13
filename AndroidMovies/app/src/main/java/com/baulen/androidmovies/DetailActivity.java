861a3b33e792787c19645706829cb9902bf8354c
Sejas <antonio@sejas.es>
2017-05-13 13:52:06 +0100

    adding detail activity

--- AndroidMovies/app/src/main/java/com/baulen/androidmovies/DetailActivity.java
+++ AndroidMovies/app/src/main/java/com/baulen/androidmovies/DetailActivity.java
@@ -2,18 +2,21 @@ package com.baulen.androidmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 /**
  * Created by macbookpro on 3/4/17.
  */
 
 public class DetailActivity extends AppCompatActivity {
-
-    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
-
+    public ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mImageView =  (ImageView) findViewById(R.id.iv_poster);
+
+
     }
 }
